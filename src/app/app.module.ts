1998cec1d13a1a085b506602c02512927eeeac6d
agrushkevich <taiton252005@gmail.com>
2018-01-15 12:55:16 +0300

    forRoot() для тестирвоания пагинатора

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {SwsPaginationModule} from '../../libs/sws-pagination/src/sws-pagination.module';
+import {RouterModule} from '@angular/router';
 
 
 @NgModule({
@@ -10,9 +12,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([]),
+    SwsPaginationModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
