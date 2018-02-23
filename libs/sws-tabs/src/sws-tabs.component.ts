f73ae0eed946d89de8115a9a5c886837fdef4406
agrushkevich <taiton252005@gmail.com>
2018-02-23 16:50:39 +0300

    Дорабокта tab-ов (добавления класса)

--- libs/sws-tabs/src/sws-tabs.component.ts
+++ libs/sws-tabs/src/sws-tabs.component.ts
@@ -1,5 +1,5 @@
 import {
-  Component, OnInit, ContentChildren, QueryList, AfterViewInit,
+  Component, OnInit, ContentChildren, QueryList,
   Output, EventEmitter, AfterContentInit
 } from '@angular/core';
 import {SwsTabComponent} from './tab/sws-tab.component';
@@ -9,7 +9,7 @@ import {SwsTabComponent} from './tab/sws-tab.component';
   templateUrl: './sws-tabs.component.html',
   styleUrls: ['./sws-tabs.component.css']
 })
-export class SwsTabsComponent implements OnInit, AfterViewInit, AfterContentInit {
+export class SwsTabsComponent implements OnInit, AfterContentInit {
 
   @ContentChildren(SwsTabComponent) tabs: QueryList<SwsTabComponent> = new QueryList<SwsTabComponent>();
   @Output() clickIndex: EventEmitter<number> = new EventEmitter();
@@ -20,20 +20,14 @@ export class SwsTabsComponent implements OnInit, AfterViewInit, AfterContentInit
   ngOnInit() {
   }
 
-  ngAfterViewInit(): void {
-
-  }
-
   ngAfterContentInit(): void {
-    const activeTabs = this.tabs.filter((tab) => tab.active);
+    const activeTabs = this.tabs.filter((tab: SwsTabComponent) => tab.active);
     if (activeTabs.length === 0) {
-      this.selectTab(this.tabs.first, 0);
+      this.selectTab(this.tabs.first);
     }
   }
 
-  selectTab(tab: SwsTabComponent, index: number) {
-    console.log('click index', index);
-    console.log('tab', tab);
+  selectTab(tab: SwsTabComponent) {
     this.tabs.forEach((elemTab: SwsTabComponent) => {
       if (elemTab === tab) {
         elemTab.active = true;
