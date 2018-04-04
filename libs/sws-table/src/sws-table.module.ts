af6004f8838fb0350c968df77521022f01fbd763
agrushkevich <taiton252005@gmail.com>
2018-04-04 17:34:33 +0300

    ngb paginator + table

--- libs/sws-table/src/sws-table.module.ts
+++ libs/sws-table/src/sws-table.module.ts
@@ -2,19 +2,13 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {SwsTableComponent} from './sws-table.component';
 import {HttpClientModule} from '@angular/common/http';
-// import {SwsLoadingModule} from '../../sws-loading/src/sws-loading.module';
-// import {SwsPaginationModule} from '../../sws-pagination/src/sws-pagination.module';
 import {SwsLoadingModule} from 'sws-loading';
 import {SwsPaginationModule} from 'sws-ngb-paginator';
-// import {SwsPaginationModule} from '../../sws-ng-pagination/src/sws-pagination.module';
-// import {SwsPaginationModule} from 'sws-pagin';
-// import {NgbPaginationModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   imports: [
     CommonModule,
     HttpClientModule,
-    // SwsPaginationModule,
     SwsLoadingModule,
     SwsPaginationModule
   ],
